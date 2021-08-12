9d7722f9519507e4ebd4473de5708de5c94834ab
Rohit <rohitsrao@gmail.com>
2021-08-12 20:32:03 +0200

    Defined a map on DUMMY_CATEGORIES to generate cateorgy widgets

--- 03_meals_app/lib/screens/categories.dart
+++ 03_meals_app/lib/screens/categories.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
+import './dummy_data.dart';
+import './category_item.dart';;
+
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return (
       GridView(
-        children: <Widget>[
-        ],
+        children: DUMMY_CATEGORIES.map(
+          (categoryData) => CategoryItem(
+            categoryData.title,
+            categoryData.color
+          )
+        ).toList(),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
           childAspectRatio: 3/2,
