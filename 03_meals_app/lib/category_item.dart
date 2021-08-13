2c364fa8f4384853819b5b6b10d4d4938c3e2996
Rohit <rohitsrao@gmail.com>
2021-08-13 08:20:33 +0200

    Tapping on Category Item takes to new screen
    Used Navigator to push a page onto the stack.
    To push we use a builder method that returns an instance of the Screen
    Introduced InkWell into the GridView so that taps can be detected
    InkWell also has option to set a splash effect
    Created Category Meals Screen

--- 03_meals_app/lib/category_item.dart
+++ 03_meals_app/lib/category_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './screens/category_meals.dart';
+
 class CategoryItem extends StatelessWidget {
 
   final String title;
@@ -10,25 +12,40 @@ class CategoryItem extends StatelessWidget {
     this.color
   );
 
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).push(
+      MaterialPageRoute(
+          builder: (_) {
+            return CategoryMealsScreen();
+          }
+      )
+    );
+  }
+
   @override
   Widget build(BuildContext context){
     return (
-      Container(
-        padding: const EdgeInsets.all(15),
-        child:  Text(
-          title,
-          style: Theme.of(context).textTheme.title,
-        ),
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              color.withOpacity(0.7),
-              color
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight
+      InkWell(
+        onTap: () => selectCategory(context),
+        splashColor: Theme.of(context).primaryColor,
+        borderRadius: BorderRadius.circular(15),
+        child: Container(
+          padding: const EdgeInsets.all(15),
+          child:  Text(
+            title,
+            style: Theme.of(context).textTheme.title,
           ),
-          borderRadius: BorderRadius.circular(15)
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                color.withOpacity(0.7),
+                color
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight
+            ),
+            borderRadius: BorderRadius.circular(15)
+          )
         )
       )
     );
