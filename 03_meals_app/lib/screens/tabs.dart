1e3a1d67a8073388988bbdb4caa7ec1f1eecc3bc
Rohit <rohitsrao@gmail.com>
2021-08-17 18:48:28 +0200

    Shifted the tabs to the bottom of the app

--- 03_meals_app/lib/screens/tabs.dart
+++ 03_meals_app/lib/screens/tabs.dart
@@ -5,40 +5,56 @@ import './favorites.dart';
 
 class TabsScreen extends StatefulWidget {
   @override
-  TabsScreenState createState() => TabsScreenState();
+  _TabsScreenState createState() => _TabsScreenState();
 }
 
-class TabsScreenState extends State<TabsScreen>{
+class _TabsScreenState extends State<TabsScreen>{
+
+  final List<Map<String, Object>> _pages = [
+    {
+      'page': CategoriesScreen(),
+      'title': 'Categories',
+    },
+    {
+      'page': FavoritesScreen(),
+      'title': 'Favorites'
+    }
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index){
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return (
-      DefaultTabController(
-        length: 2,
-        initialIndex: 0,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text(
-              'Meals'
-            ),
-            bottom: TabBar(
-              tabs: <Widget>[
-                Tab(
-                  icon: Icon(Icons.category),
-                  text: 'Categories',
-                ),
-                Tab(
-                  icon: Icon(Icons.star),
-                  text: 'Favorites',
-                ),
-              ]
-            ),
+      Scaffold(
+        appBar: AppBar(
+          title: Text(
+            _pages[_selectedPageIndex]['title']
           ),
-          body: TabBarView(
-            children: <Widget>[
-              CategoriesScreen(),
-              FavoritesScreen(),
-            ]
-          ),
+        ),
+        body: _pages[_selectedPageIndex]['page'],
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: _selectPage,
+          backgroundColor: Theme.of(context).primaryColor,
+          selectedItemColor: Theme.of(context).accentColor,
+          unselectedItemColor: Colors.white,
+          currentIndex:  _selectedPageIndex,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.category),
+              title: Text('Categories')
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.star),
+              title: Text('Favorites'),
+            ),
+          ]
         ),
       )
     );
