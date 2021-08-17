438bb7915a36e8dcd72a92da2460293ddcaf966b
Rohit <rohitsrao@gmail.com>
2021-08-17 12:06:30 +0200

    Displayed duration and complexity for each recipe in category
    Defined a getter for Complexity
    After the stack we now have a padding which wraps a row
       This row contains 2 row children
          1 row child for duration
          1 row child for complexity
    Both row children have an icon and a text
    SpaceAround used in parent row to have them equally spaced

--- 03_meals_app/lib/widgets/meal_item.dart
+++ 03_meals_app/lib/widgets/meal_item.dart
@@ -18,6 +18,22 @@ class MealItem extends StatelessWidget {
     @required this.affordability,
   });
 
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Simple:
+        return 'Simple';
+        break;
+      case Complexity.Challenging:
+        return 'Challenging';
+        break;
+      case Complexity.Hard:
+        return 'Hard';
+        break;
+      default: 
+        return 'Unknown';
+    }
+  }
+
   void selectMeal() {
   }
 
@@ -70,6 +86,36 @@ class MealItem extends StatelessWidget {
                     ),
                   ),
                 ],
+              ),
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.schedule,
+                        ),
+                        SizedBox(
+                          width: 6,
+                        ),
+                        Text('$duration min'),
+                      ]
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.work,
+                        ),
+                        SizedBox(
+                          width: 6,
+                        ),
+                        Text(complexityText),
+                      ]
+                    ),
+                  ]
+                ),
               ),
             ]
           ),
