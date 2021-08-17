946b2c960375ec0132cf7f6c6f476eaacbeadbbb
Rohit <rohitsrao@gmail.com>
2021-08-17 10:32:07 +0200

    Created Meal model in meal.dart
    Updated dummy_data.dart to now contain meal recipes also

--- 03_meals_app/lib/screens/category_meals.dart
+++ 03_meals_app/lib/screens/category_meals.dart
@@ -19,10 +19,10 @@ class CategoryMealsScreen extends StatelessWidget {
         appBar: AppBar(
           title: Text(categoryTitle) 
         ),
-        body: Center(
-          child: Text(
-            'Recipes'
-          )
+        body: ListView.builder(
+          itemBuilder: (ctx, index) {
+          },
+          itemCount: 
         )
       )
     );
