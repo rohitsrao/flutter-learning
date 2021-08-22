6078f8e4e64b2603fb5a73e6cb0221ae3326342e
Rohit <rohitsrao@gmail.com>
2021-08-22 08:01:34 +0200

    Display product overview grid

---
+++ 04_shop/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../screens/products_overview.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProductsOverviewScreen(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MyShop'),
+      ),
+      body: Center(
+        child: Text('Let\'s build a shop!'),
+      ),
+    );
+  }
+}
