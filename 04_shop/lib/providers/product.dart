8de73b34cc1774e8efb5a4585bbfe0714453c4d5
Rohit <rohitsrao@gmail.com>
2021-09-24 21:23:33 +0200

    Favorite status updated on backend as well

--- 04_shop/lib/providers/product.dart
+++ 04_shop/lib/providers/product.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
 
@@ -18,9 +20,32 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavoriteStatus() {
+  void _setFavoriteValue(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavoriteStatus() async {
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    final url = Uri.parse(
+      'https://flutter-update-39d4f-default-rtdb.europe-west1.firebasedatabase.app/products/$id.json'
+    );
+    try {
+      final response = await http.patch(
+        url,
+        body: json.encode({
+          'isFavorite': isFavorite,
+        }),
+      );
+      if (response.statusCode >= 400) {
+        _setFavoriteValue(oldStatus);
+      }
+    }
+    catch (error) {
+      _setFavoriteValue(oldStatus);
+    }
   }
 
 }
