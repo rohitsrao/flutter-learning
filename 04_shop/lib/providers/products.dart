6ec2c5dd300d3a283d30073ceaafa773449dcbf7
Rohit <rohitsrao@gmail.com>
2021-09-24 20:51:15 +0200

    Optimistic deletion of product from manage products screen

--- 04_shop/lib/providers/products.dart
+++ 04_shop/lib/providers/products.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 import './product.dart';
+import '../models/http_exception.dart';
 
 class Products with ChangeNotifier {
 
@@ -129,9 +130,20 @@ class Products with ChangeNotifier {
     }
   }
 
-  void deleteProduct(String id) {
-    _items.removeWhere((prod) => prod.id == id);
+  Future<void> deleteProduct(String id) async {
+    final url = Uri.parse(
+      'https://flutter-update-39d4f-default-rtdb.europe-west1.firebasedatabase.app/products/$id.json'
+    );
+    final existingProductIndex = _items.indexWhere((prod) => prod.id == id);
+    var existingProduct = _items[existingProductIndex];
+    _items.removeAt(existingProductIndex);
     notifyListeners();
+    final response = await http.delete(url);
+    if (response.statusCode >= 400) {
+      _items.insert(existingProductIndex, existingProduct);
+      notifyListeners();
+      throw HttpException('Could not delete product');
+    }
   }
-
 }
+
