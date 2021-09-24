380b5b98994ea64e4824488bbd67f5c70cd7e71d
Rohit <rohitsrao@gmail.com>
2021-09-24 12:07:31 +0200

    Added a basic post request to firebase

--- 04_shop/lib/providers/products.dart
+++ 04_shop/lib/providers/products.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import './product.dart';
 
@@ -52,6 +54,17 @@ class Products with ChangeNotifier {
   }
 
   void addProduct(Product product) {
+    final url = Uri.parse('https://flutter-update-39d4f-default-rtdb.europe-west1.firebasedatabase.app/products.json');
+    http.post(
+      url,
+      body: json.encode({
+        'title': product.title,
+        'description': product.description,
+        'imageUrl': product.imageUrl,
+        'price': product.price,
+        'isFavorite': product.isFavorite,
+      }),
+    );
     final newProduct = Product(
       id: DateTime.now().toString(),
       title: product.title,
