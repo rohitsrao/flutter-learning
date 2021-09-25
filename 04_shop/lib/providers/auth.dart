73bc80a616188dc4daa8f4d0e0f06b62c1f61822
Rohit <rohitsrao@gmail.com>
2021-09-25 16:50:32 +0200

    Error handling for login and sign up added

--- 04_shop/lib/providers/auth.dart
+++ 04_shop/lib/providers/auth.dart
@@ -3,6 +3,7 @@ import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 
 import '../config.dart';
+import '../models/http_exception.dart';
 
 class Auth with ChangeNotifier {
 
@@ -14,15 +15,23 @@ class Auth with ChangeNotifier {
     final url = Uri.parse(
       'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=' + firebaseAPIKey
     );
-    final response = await http.post(
-      url,
-      body: json.encode({
-        'email': email,
-        'password': password,
-        'returnSecureToken': true,
-      }),
-    );
-    print(json.decode(response.body));
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode({
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        }),
+      );
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    }
+    catch (error) {
+      throw error;
+    }
   }
 
   Future<void> signup(String email, String password) async {
