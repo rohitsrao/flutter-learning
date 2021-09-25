2900a00222cefd40afced23f334fcbb0f7069084
Rohit <rohitsrao@gmail.com>
2021-09-25 07:57:00 +0200

    Placing an order updates database

--- 04_shop/lib/providers/orders.dart
+++ 04_shop/lib/providers/orders.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 import './cart.dart';
 
@@ -25,14 +27,33 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    final url = Uri.parse(
+      'https://flutter-update-39d4f-default-rtdb.europe-west1.firebasedatabase.app/orders.json'
+    );
+    final timestamp = DateTime.now();
+    final response = await http.post(
+      url,
+      body: json.encode({
+        'amount': total,
+        'dateTime': timestamp.toIso8601String(),
+        'products': cartProducts
+          .map((cartProduct) => {
+            'id': cartProduct.id,
+            'title': cartProduct.title,
+            'quantity': cartProduct.quantity,
+            'price': cartProduct.price,
+          })
+          .toList(),
+      }),
+    );
     _orders.insert(
       0,
       OrderItem(
-        id: DateTime.now().toString(),
+        id: json.decode(response.body)['name'],
         amount: total,
         products: cartProducts,
-        dateTime: DateTime.now(),
+        dateTime: timestamp,
       ),
     );
     notifyListeners();
