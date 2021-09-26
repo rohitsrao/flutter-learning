84133b421fafc6e466c16101d12facfaa9197e1f
Rohit <rohitsrao@gmail.com>
2021-09-26 21:55:10 +0200

    Added animation for product images in products overview

--- 04_shop/lib/widgets/product_item.dart
+++ 04_shop/lib/widgets/product_item.dart
@@ -18,8 +18,9 @@ class ProductItem extends StatelessWidget {
         borderRadius: BorderRadius.circular(10),
         child: GridTile(
           child: GestureDetector(
-            child: Image.network(
-              product.imageUrl,
+            child: FadeInImage(
+              placeholder: AssetImage('assets/images/product-placeholder.png'),
+              image: NetworkImage(product.imageUrl),
               fit: BoxFit.cover,
             ),
             onTap: () {
