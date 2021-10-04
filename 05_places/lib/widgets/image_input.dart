04be40f131d23d7bd941c00921458f879ce0fdd5
Rohit <rohitsrao@gmail.com>
2021-10-04 22:59:32 +0200

    Created ImageInput widget that can access device camera

--- 05_places/lib/widgets/image_input.dart
+++ 05_places/lib/widgets/image_input.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   @override
@@ -9,6 +10,14 @@ class ImageInput extends StatefulWidget {
 class _ImageInputState extends State<ImageInput>{
 
   File? _storedImage;
+
+  Future<void> _takePicture() async {
+    final picker = ImagePicker();
+    final imageFile = await picker.getImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,8 +51,7 @@ class _ImageInputState extends State<ImageInput>{
               icon: Icon(Icons.camera),
               label: Text('Take Picture'),
               textColor: Theme.of(context).primaryColor,
-              onPressed: () {
-              }
+              onPressed: _takePicture,
             ),
           ),
         ]
