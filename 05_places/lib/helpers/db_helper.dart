a7ee2ff158f6500d2f6c6033b4d4bbb9a8c12ae1
Rohit <rohitsrao@gmail.com>
2021-10-05 11:36:16 +0200

    Can save and retrieve data from on device database

--- 05_places/lib/helpers/db_helper.dart
+++ 05_places/lib/helpers/db_helper.dart
@@ -3,20 +3,28 @@ import 'package:sqflite/sqflite.dart' as sql;
 
 class DBHelper {
 
-  static Future<void> insert(String table, Map<String, Object> data) async {
+  static Future<sql.Database> database() async {
     final dbPath = await sql.getDatabasesPath();
-    final sqlDb = await sql.openDatabase(
-      path.join(dbPath, 'places.db'),
+    return sql.openDatabase(
+      path.join(dbPath, 'user_places.db'),
       onCreate: (db, version) {
         return db.execute('CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT)');
       },
       version: 1,
     );
+  }
 
-    await sqlDb.insert(
+  static Future<void> insert(String table, Map<String, Object> data) async {
+    final db = await DBHelper.database();
+    await db.insert(
       table, 
       data, 
-      conflictAlgorithm: ConflictAlgorithm.replace,
+      conflictAlgorithm: sql.ConflictAlgorithm.replace,
     );
+  }
+
+  static Future<List<Map<String, dynamic>>> getData(String table) async {
+    final db = await DBHelper.database();
+    return db.query(table);
   }
 }
--- 05_places/lib/screens/places_list_screen.dart
+++ 05_places/lib/screens/places_list_screen.dart
@@ -20,23 +20,30 @@ class PlacesListScreen extends StatelessWidget {
             )
           ]
         ),
-        body: Consumer<GreatPlaces>(
-          child: Center(
-            child: const Text('Got no places yet, start adding some!'),
-          ),
-          builder: (context, greatPlaces, child) => greatPlaces.items.length <= 0 
-            ? child as Widget
-            : ListView.builder(
-                itemCount: greatPlaces.items.length,
-                itemBuilder: (context, i) => ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: FileImage(greatPlaces.items[i].image)
-                  ),
-                  title: Text(greatPlaces.items[i].title),
-                  onTap: () {},
-                )
+        body: FutureBuilder(
+          future: Provider.of<GreatPlaces>(context, listen: false).fetchAndSetPlaces(),
+          builder: (context, snapshot) => snapshot.connectionState == ConnectionState.waiting 
+            ? Center(
+                child: CircularProgressIndicator(),
               )
-        )
+            : Consumer<GreatPlaces>(
+              child: Center(
+                child: const Text('Got no places yet, start adding some!'),
+              ),
+              builder: (context, greatPlaces, child) => greatPlaces.items.length <= 0 
+                ? child as Widget
+                : ListView.builder(
+                    itemCount: greatPlaces.items.length,
+                    itemBuilder: (context, i) => ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: FileImage(greatPlaces.items[i].image)
+                      ),
+                      title: Text(greatPlaces.items[i].title),
+                      onTap: () {},
+                    )
+                  )
+              )
+        ),
       )
     );
   }
